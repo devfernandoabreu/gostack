e7a09500d5cd33fe8a42baffe24ea316d0ddbbbb
devfernandoabreu <devfernandoabreu@gmail.com>
2020-11-26 21:45:39 -0300

    Configurando Eslint, prettier e Editor Config

--- backend/primeiro-projeto-node/src/server.ts
+++ backend/primeiro-projeto-node/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello go stack' });
+app.get("/", (request, response) => {
+  return response.json({ message: "Online" });
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log("Server started on port 3333");
 });
